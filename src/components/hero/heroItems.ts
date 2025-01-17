1dba80b5d70c8b12e9f90741312901ddf7bdf65a
moatasim <mo32000a@gmail.com>
2025-01-17 18:44:20 +0200

    added Al-Assema project

--- src/components/hero/heroItems.ts
+++ src/components/hero/heroItems.ts
@@ -2,7 +2,8 @@ import { StaticImageData } from "next/image"
 
 import image1 from "../../../public/assets/images/thrive-ai.webp"
 import image2 from "../../../public/assets/images/triple-clean.webp"
-import image3 from "../../../public/assets/images/smart-generation.webp"
+import image3 from "../../../public/assets/images/al-assema.webp"
+import image4 from "../../../public/assets/images/smart-generation.webp"
 import image14 from "../../../public/assets/images/logos/html-5-logo.svg"
 import image15 from "../../../public/assets/images/logos/Official_CSS_new_Logo.png"
 import image16 from "../../../public/assets/images/logos/logo-javascript.svg"
@@ -114,11 +115,55 @@ const heroItems : HeroItemT[] = [
         itemColor: "#405ca8"
     },{
         id: "3",
+        title: "Al-Assema Compressor",
+        description: "Al Assema Company is one of the leading companies specializing in compressed air technology, integrated engineering and industrial solutions, and production line equipment.",
+        note: "",
+        href: "https://alassemacompressor.com/en",
+        src: image3,
+        alt: "Al-Assema Compressors project image",
+        techUsed: [
+            {
+                id: "1",
+                name: "Next.js",
+                imgSrc: image20
+            },
+            {
+                id: "2",
+                name: "React.js",
+                imgSrc: image19
+            },
+            {
+                id: "3",
+                name: "HTML",
+                imgSrc: image14
+            },
+            {
+                id: "4",
+                name: "CSS",
+                imgSrc: image15
+            },
+            {
+                id: "5",
+                name: "TypeScript",
+                imgSrc: image21
+            },
+            {
+                id: "6",
+                name: "Github",
+                imgSrc: image17
+            },
+        ],
+        clientStars: [1,2,3,4,5],
+        clientRatingAr: "خمس نجوم في التعامل والجودة ووقت التنفيذ والاحترافية في التعامل",
+        clientRatingEn: "Five stars for communication, quality, delivery time, and professionalism",
+        itemColor: "#14365c"
+    },{
+        id: "4",
         title: "Smart Generation",
         description: "An introductory website for the Egyptian company Smart Generation, a leader in the fields of engineering, construction, engineering supplies, low-current systems, central cooling and air conditioning, plumbing works, and more.",
         note: "",
         href: "https://smart-generation-bay.vercel.app/",
-        src: image3,
+        src: image4,
         alt: "Smart Generation project image",
         techUsed: [
             {
@@ -153,8 +198,8 @@ const heroItems : HeroItemT[] = [
             },
         ],
         clientStars: [1,2,3,4,5],
-        clientRatingAr: "فنان ما شاء الله",
-        clientRatingEn: "An artist, Mashallah.",
+        clientRatingAr: "",
+        clientRatingEn: "",
         itemColor: "#e8af06"
     }
 ]
